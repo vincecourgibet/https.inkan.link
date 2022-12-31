3ccb28283ecd3c3e40342efb7b969a22005881ac
thomnico <thomnico@gmail.com>
2022-12-31 17:43:34 +0100

    Basic changes before change of style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'colors.indigo',
+        primary: 'colors.red',
         secondary: 'colors.yellow',
         neutral: 'colors.gray',
       },
